2148186bfbc732b4732e18485459736178995a56
Daniel Lockhart <daniel@lockyy.com>
2016-08-30 01:47:40 +0100

    Add StyleRoot wrapper to Routes. Add Responsive hiding css constants to css.js
    Styleroot is needed for media queries in Radium

--- src/constants/styles/css.js
+++ src/constants/styles/css.js
@@ -3,6 +3,16 @@ import Colors from './colors'
 let css = {
   link: {
     color: Colors.green.lighter,
+  },
+  mobile: {
+    '@media (min-device-width: 993px)': {
+      display: 'none',
+    },
+  },
+  desktop: {
+    '@media (max-device-width: 992px)': {
+      display: 'none',
+    },
   }
 }
 
--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import DocumentTitle from 'react-document-title'
 import Strings from '../constants/strings'
+import {StyleRoot} from 'radium';
 import App from './App';
 import ScenarioContainer from './Scenario'
 import NotFound from '../components/NotFound'
@@ -13,14 +14,16 @@ module.exports = class Routes extends Component {
   render() {
     return (
       <DocumentTitle title={Strings.rootPageTitle}>
-        <Router history={browserHistory} >
-          <Route path="/" component={App}>
-            <IndexRoute component={ScenarioContainer}/>
-            <Route path="generators/:id" component={ScenarioContainer} />
-            <Route path="generators/:id/scenario/:uuid" component={ScenarioContainer} />
-            <Route path="*" component={NotFound} />
-          </Route>
-        </Router>
+        <StyleRoot>
+          <Router history={browserHistory} >
+            <Route path="/" component={App}>
+              <IndexRoute component={ScenarioContainer}/>
+              <Route path="generators/:id" component={ScenarioContainer} />
+              <Route path="generators/:id/scenario/:uuid" component={ScenarioContainer} />
+              <Route path="*" component={NotFound} />
+            </Route>
+          </Router>
+        </StyleRoot>
       </DocumentTitle>
     );
   }
