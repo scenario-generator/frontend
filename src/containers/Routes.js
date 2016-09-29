5a2fd665136b315f47059267a13ac80a54aba7a4
Daniel Lockhart <daniel@lockyy.com>
2016-09-30 00:37:00 +0100

    Add google analytics

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react'
-import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router'
-import DocumentTitle from 'react-document-title'
-import Strings from '../constants/strings'
+import React, { Component } from 'react';
+import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
+import ReactGA from 'react-ga';
+import DocumentTitle from 'react-document-title';
+import Strings from '../constants/strings';
 import {StyleRoot} from 'radium';
 import App from './App';
-import ScenarioContainer from './Scenario'
-import NotFound from '../components/NotFound'
+import ScenarioContainer from './Scenario';
+import NotFound from '../components/NotFound';
+import ENV from '../../env'
 
 /**
  * Component is exported for conditional usage in Root.js
  */
 module.exports = class Routes extends Component {
+  componentDidMount() {
+    ReactGA.initialize(ENV.analytics.google);
+  }
+
+  logPageView() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
       <DocumentTitle title={Strings.rootPageTitle}>
         <StyleRoot>
-          <Router history={browserHistory} >
+          <Router history={browserHistory} onUpdate={this.logPageView}>
             <Route path="/" component={App}>
               <IndexRoute component={ScenarioContainer}/>
               <Route path="generators/:id" component={ScenarioContainer} />
